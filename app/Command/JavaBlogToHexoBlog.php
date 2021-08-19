a5403e9ec34267f3aefca28a91c53ca50967cc5d
liangguifeng <1476982312@qq.com>
2021-08-19 19:59:37 +0800

    [feat]优化

--- app/Command/JavaBlogToHexoBlog.php
+++ app/Command/JavaBlogToHexoBlog.php
@@ -20,11 +20,18 @@ class JavaBlogToHexoBlog extends HyperfCommand
     protected $container;
 
     /**
-     * 定义文件存储路径
+     * 定义发布文件存储路径
      *
      * @var string
      */
-    protected $path = '/Users/liang/www/fanerblog-tool/markdown/';
+    protected $publishPath = '/Users/liang/web/fanerblog/source/_posts/';
+
+    /**
+     * 定义未发布存储路径
+     *
+     * @var string
+     */
+    protected $noPublishPath = '/Users/liang/web/fanerblog/source/_drafts/';
 
     /**
      * JavaBlogToHexoBlog constructor.
@@ -51,11 +58,16 @@ class JavaBlogToHexoBlog extends HyperfCommand
     public function handle()
     {
         Article::query()
+            ->where('is_markdown', true)
             ->orderBy('create_time')
             ->chunkById(20, function ($articles) {
                 foreach ($articles as $article) {
                     try {
-                        $articleFile = fopen($this->path . $article->title . '.md', "w");
+                        if ($article->status) {
+                            $articleFile = fopen($this->publishPath . $article->title . '.md', "w");
+                        } else {
+                            $articleFile = fopen($this->noPublishPath . $article->title . '.md', "w");
+                        }
 
                         $type = $article->type->name ?? '未分类';
 
@@ -66,16 +78,37 @@ class JavaBlogToHexoBlog extends HyperfCommand
 
                         $tags = rtrim($tags, ',');
 
-                        //定义标题
-                        $title = "---
+                        if ($article->status) {
+                            //定义标题
+                            $title = "---
 title: $article->title
 date: $article->create_time
 categories: ['$type']
+toc: true
 tags: [$tags]
 cover: $article->cover_image
 ---
 
 ";
+                        } else {
+                            //定义标题
+                            $title = "---
+title: $article->title
+date: $article->create_time
+categories: ['$type']
+toc: true
+tags: [$tags]
+cover: $article->cover_image
+password: fanerblog
+abstract: 这里有加密的东西，需要密码才能继续阅读！
+message: 该文章已加密，请输入阅读密码！
+wrong_pass_message: 您输入的密码错误，请检查并重试。
+---
+
+";
+                        }
+
+
                         //写入标题
                         fwrite($articleFile, $title);
                         //写入markdown内容
